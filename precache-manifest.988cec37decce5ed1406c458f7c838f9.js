5d20d3f96c31f8304e1db04d8dbedf3724335a51
River2056 <45826383+River2056@users.noreply.github.com>
2019-08-23 23:31:26 +0800

    Initial commit

---
+++ precache-manifest.988cec37decce5ed1406c458f7c838f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31198178397f16821f5f541207a8c993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73544ebb3ac48688d1f6",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "0fbd1fd971c4d1950cd6",
+    "url": "/static/css/main.d8e51d2c.chunk.css"
+  },
+  {
+    "revision": "73544ebb3ac48688d1f6",
+    "url": "/static/js/2.3e37a4d1.chunk.js"
+  },
+  {
+    "revision": "0fbd1fd971c4d1950cd6",
+    "url": "/static/js/main.fcab9288.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
